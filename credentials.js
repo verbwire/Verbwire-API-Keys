6b2dabe1be859644fe0fa4e6b826723bc3d55507
Anushrey Jain <anushrxy@gmail.com>
2023-07-22 20:48:21 +0530

    Update credentials.js

--- credentials.js
+++ credentials.js
@@ -2,14 +2,8 @@ export const credentials = {
 
   privateKey: "YOUR_PRIVATE_KEY",
 
-  privateKey: "YOUR_PRIVATE_KEY",
-
 //   Unrestricted Private Keys have a scope added to them,
 //   Which is used to determine what the key can access.
-//   Add Scope : /v1/nft/data/owned
-
-  publicKeyUnrestricted: "YOUR_PUBLIC_KEY",
- 
 //   Add Scope : /v1/nft/data/owned
 
   publicKeyUnrestricted: "YOUR_PUBLIC_KEY",
@@ -18,10 +12,6 @@ export const credentials = {
 //    Restricted Private Keys have a scope and restriction added to them,
 //    Which is used to determine what the key can access.
 //    Add Scope :  /v1/nft/data/isWalletHolderOfToken
-//    Add Restriction:  
-//                     walletAddress  "Your_Wallet_Address"
-
-  publicKeyRestricted: "YOUR_ANOTHER_PUBLIC_KEY ",
 //                     walletAddress  "Your_Wallet_Address"
 
   publicKeyRestricted: "YOUR_ANOTHER_PUBLIC_KEY ",
